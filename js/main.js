12278bbfb5ee25e3af8f61ea0dcfe06ff4b130fa
Davide Saporita <davide.saporita@gmail.com>
2020-03-30 16:30:08 +0200

    CSS Styling (basic)
    JS Dice Game still in console

--- js/main.js
+++ js/main.js
@@ -11,35 +11,50 @@
  * 
  */
 
-// Creazione array con liste email ricevute  dal cliente
-var emailVip = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
-// Raccolta dato email dall'utente
-var emailUser = prompt('Digita la tua email come fai sempre (es. peppino@google.it) e premi OK:');
-// Flag per memorizzare in una variabile la presenza dell'email dell'utente  nella lista Vip
-var emailFound = false;
 
-// Controllo accessi
-for( var i=0; i < emailVip.length; i++) {
-    if (emailVip[i] == emailUser) {
-        emailFound = true;
-        console.log('Sei un Vip');
-    }
-}
+// All'azione click sul bottone "Clicca qui" raccogliamo in una variabile l'email inserita
+var emailButton = document.getElementById('emailButton');
+emailButton.addEventListener('click',
+    function () {
+        // Creazione array con liste email ricevute  dal cliente
+        var emailVip = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
 
-if ( emailFound == true ) {
-    // Generazione numeri random per il gioco dei dadi
-    console.log('Sei un grande, puoi giocare!');
-    var userDice = Math.ceil(Math.random() * 12);
-    console.log('Hai tirato il tuo dado, hai fatto: ' + userDice);
-    var computerDice = Math.ceil(Math.random() * 12);
-    console.log('Il computer risponde con: ' + computerDice);
-    if ( userDice > computerDice ) {
-        console.log('Complimenti! Hai vinto un buono di 5.000 lire per l\'acquisto della torre Eiffel');
-    } else if ( userDice < computerDice ) {
-        console.log('Ci dispiace tantissimo, ma con questa sconfitta hai perso tutto: casa, fortuna, denaro, moglie, marito, etc.');
-    } else {
-        console.log('Hai pareggiato, sei forte quanto un computer!');
+        // Raccolta dato email dall'utente
+        var emailUser;
+
+        // Flag per memorizzare in una variabile la presenza dell'email dell'utente  nella lista Vip
+        var emailFound = false;
+
+        var emailUser = document.getElementById('emailUser').value;
+        console.log('Siamo dentro! Con l\'email: ' + emailUser);
+
+        // Controllo accessi
+        for( var i=0; i < emailVip.length; i++) {
+            if (emailVip[i] == emailUser) {
+                emailFound = true;
+                console.log('Sei un Vip');
+            }
+        }
+
+        // Se l'utente ha superato il controllo dell'accesso, può giocare
+        if ( emailFound == true ) {
+            // Generazione numeri random per il gioco dei dadi
+            console.log('Sei un grande, puoi giocare!');
+            var userDice = Math.ceil(Math.random() * 12);
+            console.log('Hai tirato il tuo dado, hai fatto: ' + userDice);
+            var computerDice = Math.ceil(Math.random() * 12);
+            console.log('Il computer risponde con: ' + computerDice);
+            // Verifica dei risultati e feedback all'utente
+            if ( userDice > computerDice ) {
+                console.log('Complimenti! Hai vinto un buono di 5.000 lire per l\'acquisto della torre Eiffel');
+            } else if ( userDice < computerDice ) {
+                console.log('Ci dispiace tantissimo, ma con questa sconfitta hai perso tutto: casa, fortuna, denaro, moglie, marito, etc.');
+            } else {
+                console.log('Hai pareggiato, sei forte quanto un computer!');
+            }
+        } else {
+            console.log('Non vali niente, non puoi giocare. Ciaaaaoooo!');
+        }
+
     }
-} else {
-    console.log('Non vali niente, non puoi giocare. Ciaaaaoooo!');
-}
+);
